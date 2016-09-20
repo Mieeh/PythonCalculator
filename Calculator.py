5c4001393412cbfddc887c6723f2773d55cc6f68
DavidStyrbjorn <davidstyrbjorn@gmail.com>
2016-09-20 12:48:46 +0200

    Added Basic functionality to the calculator class

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,9 @@
+# Class that contains all the methods and functionality for our calculator
+# David Styrbjörn
+# davidstyrbjorn@gmailcom
+
+import math
+
 class Calculator:
     
     def add(a,b):
@@ -6,4 +12,11 @@ class Calculator:
     def sub(a,b):
         return a-b
     
-    
+    def multiply(a,b):
+        return a*b
+
+    def division(a,b):
+        return a/b
+
+    def toThePowerOf(a,b):
+        return math.pow(a,b)
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import Calculator
+import time
 
-calc = Calculator
+calc = Calculator.Calculator
 
+# Doing some calulations with our calc object
+print("1 + 1 is " + str(calc.add(1,1)))
 
-print(calc.add(1,1))
+print("3 - 2 is " + str(calc.sub(3,2)))
+
+print("4 * 4 is " + str(calc.multiply(4,4)))
+
+print("4 / 2 is " + str(calc.division(4,2)))
+
+print("3 ** 2 is " + str(calc.toThePowerOf(3,2)))
